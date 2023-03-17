9272cbf5733bcc7567482819ad2b2747e35fdbea
milindkumar64 <milindgupta@gmail.com>
2023-03-18 00:49:45 +0530

    Add: While Loop Problems are added

---
+++ src/com/bridgelabz/whileloop/ReverseNumber.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.whileloop;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter Number :");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+	    int rev=0;
+        int rem=0;
+        while(num>0)
+        {
+        	rem=num%10;
+        	rev = (rev*10)+rem;
+        	num=num/10;
+       
+        }
+		System.out.println("Reversed number is :"+rev);
+        
+	}
+
+}
---
+++ src/com/bridgelabz/whileloop/SumOfNaturalNumbers.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.whileloop;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+
+	public static void main(String[] args) {
+		
+	
+         int n = 5;
+         int i=1;
+         int sum =0;
+         while(i<=n)
+         {
+        	
+        	 sum=sum+i;
+        	 i++;
+         }
+         System.out.println("value of sum :"+sum);
+	}
+
+}
